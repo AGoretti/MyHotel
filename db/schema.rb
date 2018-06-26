331d641aa4212b2e7e566fd9e9729f5fe96cb9ee
André Goretti <andremotta96@gmail.com>
2018-06-26 11:19:38 -0300

    twitter

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_26_132625) do
+ActiveRecord::Schema.define(version: 2018_06_26_140120) do
+
+  create_table "hotels", force: :cascade do |t|
+    t.string "name"
+    t.text "adress"
+    t.text "descrition"
+    t.float "rating"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
