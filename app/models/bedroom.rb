c6c27b4976d416442afe09b2cd8ccbd47372875b
Andre Eduardo <andre.eduardo11@hotmail.com>
2018-06-30 09:09:44 -0300

    adicionando quartos na tela inicial

--- app/models/bedroom.rb
+++ app/models/bedroom.rb
@@ -1,6 +1,6 @@
 class Bedroom < ApplicationRecord
   belongs_to :hotel
-  
-  has_attached_file :foto, styles: { medium: "300x300>", thumb: "100x100>" }# default_url: "<%=image_tag"fundo.jpg"%>"
+
+  has_attached_file :foto, styles: { medium: "600x600>", thumb: "100x100>" }# default_url: "<%=image_tag"fundo.jpg"%>"
   validates_attachment_content_type :foto, content_type: /\Aimage\/.*\z/
 end
