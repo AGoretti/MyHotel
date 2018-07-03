8f6672b59104c62341f18671a25397c6990cb20f
Andre Eduardo <andre.eduardo11@hotmail.com>
2018-07-03 00:08:04 -0300

    adicionando validaçoes em hotel e bedroom

--- app/models/bedroom.rb
+++ app/models/bedroom.rb
@@ -1,6 +1,8 @@
 class Bedroom < ApplicationRecord
   belongs_to :hotel
-
   has_attached_file :foto, styles: { medium: "600x600>", thumb: "200x500>" }# default_url: "<%=image_tag"fundo.jpg"%>"
   validates_attachment_content_type :foto, content_type: /\Aimage\/.*\z/
+   validates :description, presence: true
+   validates :foto, presence: true
+   validates :price, presence: true
 end
--- app/models/hotel.rb
+++ app/models/hotel.rb
@@ -1,6 +1,7 @@
 class Hotel < ApplicationRecord
   has_many :bedrooms
-
+ validates :name, presence: true, uniqueness: true
+ validates :location, presence: true,uniqueness: true
 
 
 end
